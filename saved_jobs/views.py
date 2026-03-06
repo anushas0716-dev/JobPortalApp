8c4379183318b5d78c5f175582a62a7e6b15b9b3
Sandeep G L <sandeeplokesh44@gmail.com>
2026-03-06 16:12:34 +0530

    feat: add suggested jobs API based on skills, location and experience

--- saved_jobs/views.py
+++ saved_jobs/views.py
@@ -5,22 +5,16 @@ from candidates.models import CandidateProfile
 from .models import SavedJob
 from .serializers import SavedJobSerializer
 
-
 class SavedJobViewSet(viewsets.ModelViewSet):
     serializer_class   = SavedJobSerializer
     permission_classes = [IsAuthenticated]
     http_method_names  = ['get', 'post', 'patch', 'delete']
 
     def get_queryset(self):
-        try:
-            candidate = CandidateProfile.objects.get(user=self.request.user)
-            return SavedJob.objects.filter(
-                candidate=candidate
-            ).select_related(
-                'job', 'job__company'
-            )
-        except CandidateProfile.DoesNotExist:
+        if getattr(self, 'swagger_fake_view', False):
             return SavedJob.objects.none()
+        candidate = CandidateProfile.objects.get(user=self.request.user)
+        return SavedJob.objects.filter(candidate=candidate)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
