8604cc16391b5a33461935d58ea15984a412dd29
2000anushas-crypto <2000anushas@gmail.com>
2026-03-13 13:15:22 +0530

    Added Job Portal Authentication Module

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import JobPortalAuth from './JobPortalAuth';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<React.StrictMode><JobPortalAuth /></React.StrictMode>);
